f45eedc6a6015a99bd1c65e790c7550cde4a7113
VoronKos <kvoronin88@gmail.com>
2023-10-29 20:15:39 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Воронин Константин, 9-я когорта — Финальный проект. Инженер по тестированию плюс
+URL_SERVICE = "https://1c1bc123-ebbe-48b1-b6e5-74e8a39c3a16.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
